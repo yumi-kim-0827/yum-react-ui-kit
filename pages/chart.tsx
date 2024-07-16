958107e46a4eff7ae4060e1bc5f629baf15697e6
yumi-kim-0827 <116349476+yumi-kim-0827@users.noreply.github.com>
2024-07-17 07:34:03 +0900

    fix:Chart nivo 제거

--- pages/chart.tsx
+++ pages/chart.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ResponsivePie } from "@nivo/pie";
+// import { ResponsivePie } from "@nivo/pie";
 import styled from "styled-components";
 //Components
 import Main from "@/components/Layout/Main";
@@ -50,7 +50,7 @@ const Chart = () => {
         <Main>
           차트 인포그래픽
           <ChartWrap>
-            <ResponsivePie
+            {/* <ResponsivePie
               data={data}
               margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
               innerRadius={0.4}
@@ -165,7 +165,7 @@ const Chart = () => {
                   ],
                 },
               ]}
-            />
+            /> */}
           </ChartWrap>
         </Main>
       </Grid>
