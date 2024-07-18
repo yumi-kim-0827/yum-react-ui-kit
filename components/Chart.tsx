0c66518eee7b7082085a41321a8774e965ff2fe4
yumi-kim-0827 <116349476+yumi-kim-0827@users.noreply.github.com>
2024-07-18 23:21:40 +0900

    fix:Chart.tsx 오류 수정

---
+++ components/Chart.tsx
@@ -0,0 +1,188 @@
+import React, { useLayoutEffect } from "react";
+/* Imports */
+import * as am5 from "@amcharts/amcharts5";
+import * as am5xy from "@amcharts/amcharts5/xy";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+
+function maybeDisposeRoot(divId: string) {
+  am5.array.each(am5.registry.rootElements, function (root) {
+    if (root.dom.id == divId) {
+      root.dispose();
+    }
+  });
+}
+
+const Chart = () => {
+  useLayoutEffect(() => {
+    maybeDisposeRoot("chartdiv");
+    let root = am5.Root.new("chartdiv");
+    const chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        wheelY: "zoomX",
+      })
+    );
+
+    // 데이터
+    const data = [
+      {
+        date: new Date(2021, 0, 1).getTime(),
+        value: 100,
+        value2: 220,
+      },
+      {
+        date: new Date(2021, 0, 2).getTime(),
+        value: 320,
+        value2: 300,
+      },
+      {
+        date: new Date(2021, 0, 3).getTime(),
+        value: 216,
+        value2: 120,
+      },
+      {
+        date: new Date(2021, 0, 4).getTime(),
+        value: 150,
+        value2: 190,
+      },
+      {
+        date: new Date(2021, 0, 5).getTime(),
+        value: 156,
+        value2: 190,
+      },
+      {
+        date: new Date(2021, 0, 6).getTime(),
+        value: 199,
+        value2: 120,
+      },
+      {
+        date: new Date(2021, 0, 7).getTime(),
+        value: 114,
+        value2: 300,
+      },
+      {
+        date: new Date(2021, 0, 8).getTime(),
+        value: 269,
+        value2: 290,
+      },
+      {
+        date: new Date(2021, 0, 9).getTime(),
+        value: 190,
+        value2: 290,
+      },
+      {
+        date: new Date(2021, 0, 10).getTime(),
+        value: 380,
+        value2: 170,
+      },
+      {
+        date: new Date(2021, 0, 11).getTime(),
+        value: 250,
+        value2: 200,
+      },
+      {
+        date: new Date(2021, 0, 12).getTime(),
+        value: 110,
+        value2: 210,
+      },
+      {
+        date: new Date(2021, 0, 13).getTime(),
+        value: 185,
+        value2: 85,
+      },
+      {
+        date: new Date(2021, 0, 14).getTime(),
+        value: 105,
+        value2: 244,
+      },
+    ];
+
+    // Create Y-axis
+    let yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(root, {
+        extraTooltipPrecision: 1,
+        renderer: am5xy.AxisRendererY.new(root, {
+          minGridDistance: 30,
+        }),
+      })
+    );
+
+    // Create X-Axis
+    let xAxis = chart.xAxes.push(
+      am5xy.DateAxis.new(root, {
+        baseInterval: { timeUnit: "day", count: 1 },
+        renderer: am5xy.AxisRendererX.new(root, {
+          minGridDistance: 20,
+          cellStartLocation: 0.2,
+          cellEndLocation: 0.8,
+        }),
+      })
+    );
+
+    // Create series
+    function createSeries(name: string, field: string) {
+      var series = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          name: name,
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: field,
+          valueXField: "date",
+          tooltip: am5.Tooltip.new(root, {}),
+        })
+      );
+
+      series.strokes.template.setAll({
+        strokeWidth: 2,
+      });
+
+      series.events.on("datavalidated", function () {
+        // @ts-ignore
+        chart.get("cursor").setAll({
+          positionX: 0.99,
+          positionY: 0.4,
+          xAxis: xAxis,
+          yAxis: yAxis,
+          alwaysShow: true,
+        });
+      });
+
+      // @ts-ignore
+      series.get("tooltip").label.set("text", "[bold]{name}[/]: {valueY}");
+      series.data.setAll(data);
+    }
+
+    createSeries("서울", "value");
+    createSeries("경기", "value2");
+
+    // Add cursor
+    chart.set(
+      "cursor",
+      am5xy.XYCursor.new(root, {
+        behavior: "none",
+        xAxis: xAxis,
+      })
+    );
+
+    xAxis.set(
+      "tooltip",
+      am5.Tooltip.new(root, {
+        themeTags: ["axis"],
+      })
+    );
+
+    yAxis.set(
+      "tooltip",
+      am5.Tooltip.new(root, {
+        themeTags: ["axis"],
+      })
+    );
+
+    return () => {
+      chart.dispose();
+    };
+  }, []);
+
+  return <div id={"chartdiv"} style={{ width: "1000px", height: "500px" }} />;
+};
+
+export default Chart;
--- pages/chart.tsx
+++ pages/chart.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import { ResponsivePie } from "@nivo/pie";
 import styled from "styled-components";
-//Components
+import dynamic from "next/dynamic";
+// Components
 import Main from "@/components/Layout/Main";
 import Grid from "@/components/Layout/Grid";
 
@@ -10,162 +10,19 @@ const ChartWrap = styled.div`
   height: 500px;
 `;
 
-const data = [
-  {
-    id: "python",
-    label: "python",
-    value: 299,
-    color: "hsl(55, 70%, 50%)",
-  },
-  {
-    id: "rust",
-    label: "rust",
-    value: 253,
-    color: "hsl(72, 70%, 50%)",
-  },
-  {
-    id: "stylus",
-    label: "stylus",
-    value: 483,
-    color: "hsl(87, 70%, 50%)",
-  },
-  {
-    id: "php",
-    label: "php",
-    value: 369,
-    color: "hsl(309, 70%, 50%)",
-  },
-  {
-    id: "scala",
-    label: "scala",
-    value: 303,
-    color: "hsl(286, 70%, 50%)",
-  },
-];
+const Chart = dynamic(() => import("@/components/Chart"), {
+  loading: () => <p>로딩 중 ...</p>,
+  ssr: false,
+});
 
-const Chart = () => {
+const Infographic = () => {
   return (
     <>
       <Grid $column="1fr" $place="center">
         <Main>
           차트 인포그래픽
           <ChartWrap>
-            {/* <ResponsivePie
-              data={data}
-              margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-              innerRadius={0.4}
-              padAngle={7}
-              activeOuterRadiusOffset={8}
-              borderWidth={1}
-              borderColor={{
-                from: "color",
-                modifiers: [["darker", 0.2]],
-              }}
-              arcLinkLabelsSkipAngle={10}
-              arcLinkLabelsTextColor="#333333"
-              arcLinkLabelsThickness={2}
-              arcLinkLabelsColor={{ from: "color" }}
-              arcLabelsSkipAngle={10}
-              arcLabelsTextColor={{
-                from: "color",
-                modifiers: [["darker", 2]],
-              }}
-              defs={[
-                {
-                  id: "dots",
-                  type: "patternDots",
-                  background: "inherit",
-                  color: "rgba(255, 255, 255, 0.3)",
-                  size: 4,
-                  padding: 1,
-                  stagger: true,
-                },
-                {
-                  id: "lines",
-                  type: "patternLines",
-                  background: "inherit",
-                  color: "rgba(255, 255, 255, 0.3)",
-                  rotation: -45,
-                  lineWidth: 6,
-                  spacing: 10,
-                },
-              ]}
-              fill={[
-                {
-                  match: {
-                    id: "ruby",
-                  },
-                  id: "dots",
-                },
-                {
-                  match: {
-                    id: "c",
-                  },
-                  id: "dots",
-                },
-                {
-                  match: {
-                    id: "go",
-                  },
-                  id: "dots",
-                },
-                {
-                  match: {
-                    id: "python",
-                  },
-                  id: "dots",
-                },
-                {
-                  match: {
-                    id: "scala",
-                  },
-                  id: "lines",
-                },
-                {
-                  match: {
-                    id: "lisp",
-                  },
-                  id: "lines",
-                },
-                {
-                  match: {
-                    id: "elixir",
-                  },
-                  id: "lines",
-                },
-                {
-                  match: {
-                    id: "javascript",
-                  },
-                  id: "lines",
-                },
-              ]}
-              legends={[
-                {
-                  anchor: "bottom",
-                  direction: "row",
-                  justify: false,
-                  translateX: 0,
-                  translateY: 56,
-                  itemsSpacing: 0,
-                  itemWidth: 100,
-                  itemHeight: 18,
-                  itemTextColor: "#999",
-                  itemDirection: "left-to-right",
-                  itemOpacity: 1,
-                  symbolSize: 18,
-                  symbolShape: "circle",
-                  effects: [
-                    {
-                      on: "hover",
-                      style: {
-                        itemTextColor: "#000",
-                      },
-                    },
-                  ],
-                },
-              ]}
-            /> */}
+            <Chart />
           </ChartWrap>
         </Main>
       </Grid>
@@ -173,4 +30,4 @@ const Chart = () => {
   );
 };
 
-export default Chart;
+export default Infographic;
